b282e8921d4e322322e587858b7b9e2b69700718
Esteban Tejeda <etejedaw@hotmail.com>
2024-08-05 01:55:46 -0400

    chore: add builders function to transform input and output data with databaseName in init function

--- src/SnowflakeModel.ts
+++ src/SnowflakeModel.ts
@@ -14,8 +14,8 @@ export abstract class SnowflakeModel {
 	}
 
 	static async find(query?: QueryOptions) {
-		const select = query?.select;
-		const where = query?.where;
+		const select = this.buildSelect(query?.select);
+		const where = this.buildWhere(query?.where);
 		const limit = query?.limit;
 
 		const queryBuilder = QueryBuilder.from(this.tableName)
@@ -25,11 +25,41 @@ export abstract class SnowflakeModel {
 			.build();
 
 		await this.instance.connect();
-		return this.instance.execQuery(queryBuilder);
+		const exec = await this.instance.execQuery(queryBuilder);
+		return this.mapResults(exec);
 	}
 
 	static async findOne(query: QueryOptions) {
 		//TODO
+	}
+
+	private static buildSelect(select: string[] = []) {
+		return select.map(
+			select => `"${this.fields[select].databaseName}"` || `"${select}"`
+		);
+	}
+
+	private static buildWhere(
+		where: Record<string, any> = {}
+	): Record<string, any> {
+		return Object.keys(where || {}).reduce((acc, val) => {
+			const fieldOptions = this.fields[val];
+			const key = fieldOptions.databaseName || val;
+			const value = where![val];
+			acc[key] = value;
+			return acc;
+		}, {} as Record<string, any>);
+	}
+
+	private static mapResults(results: any[]) {
+		return results.map(result => {
+			return Object.keys(this.fields).reduce((acc, key) => {
+				const fieldOptions = this.fields[key];
+				const dbName = fieldOptions.databaseName || key;
+				acc[key] = result[dbName];
+				return acc;
+			}, {} as Record<string, any>);
+		});
 	}
 }
 
@@ -45,7 +75,7 @@ interface FieldOptions {
 
 interface QueryOptions {
 	select?: string[];
-	where?: object;
+	where?: Record<string, any>;
 	limit?: number;
 }
 
